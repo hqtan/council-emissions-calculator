e4aa42311180ec8980e8055ab8559d6e2521f8a7
hqtan <hanqing.tan@gmail.com>
2021-08-28 21:49:18 +1000

    remove `basePath` and `assetPrefix` settings... - meant for deployment to Github Pages

--- next.config.js
+++ next.config.js
@@ -3,9 +3,5 @@ const isProduction = process.env.NODE_ENV === "production";
 console.log(`[INFO]: NODE_ENV: ${process.env.NODE_ENV}`);
 
 module.exports = {
-  basePath: isProduction ? "/council-emissions-calculator-spike" : "",
-  assetPrefix: isProduction
-    ? "/council-emissions-calculator-spike"
-    : "",
   reactStrictMode: true,
 }
